3f48e9f4eea7e8ca9258a02e9253f62138d1b6c7
Saranraj-k <47328330+Saranraj-k@users.noreply.github.com>
2019-08-02 13:07:19 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+class num{
+public static void main(String[] qwe)
+{
+Scanner Scan=new
+Scanner (System.in);
+int x=20,y=50,z=7;
+int i;
+for(i=x;i<y;i++)
+{
+if(i%7==0)
+{
+System.out.println(i);
+}
+}
+}
+}
