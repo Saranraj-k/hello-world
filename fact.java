f9e2d4e9841a95dd0c03b7ba1ab14bafa60c39f7
Saranraj-k <47328330+Saranraj-k@users.noreply.github.com>
2019-08-02 13:05:20 +0530

    Create fact.java

---
+++ fact.java
@@ -0,0 +1,16 @@
+import  java.util.Scanner;
+class fact{
+public static void main(String[] qwe)
+{
+Scanner Scan=new
+Scanner (System.in);
+int i;
+int count=1;
+int N=Scan.nextInt( );
+for(i=1;i<=N;i++)
+{
+count=count*i;
+}
+System.out.println(count);
+}
+}
